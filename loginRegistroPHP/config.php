85f818e3633c86e4336a87da71caaa0d69db0055
Aramirez25 <aramirez27tovar@outlook.es>
2021-12-21 21:18:27 +0100

    Lista TODO con php y mysql prueba

--- loginRegistroPHP/config.php
+++ loginRegistroPHP/config.php
@@ -5,7 +5,7 @@ define ('HOST', 'localhost');
 define ('DATABASE', 'test');
 
 try{
-    $connection = new PDO ("mysql:host=".HOST.";dbname=".lista_tareas, USER, PASSWORD);
+    $connection = new PDO ("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
