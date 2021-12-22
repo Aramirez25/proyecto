302b1c0246cd1d3c40ef91555c3d8c140a503079
Aramirez25 <aramirez27tovar@outlook.es>
2021-12-22 16:43:27 +0100

    más cambios

--- a easy to do list/index.php
+++ a easy to do list/index.php
@@ -15,7 +15,14 @@
 			mysqli_query($db, $sql);
 			header('location: index.php');
 		}
-	}	?>
+	}
+	// delete task
+if (isset($_GET['del_task'])) {
+	$id = $_GET['del_task'];
+
+	mysqli_query($db, "DELETE FROM tasks WHERE id_tasks=".$id);
+	header('location: index.php');
+}	?>
 <!DOCTYPE html>
 <html>
 <head>
